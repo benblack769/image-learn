9212f56eb19e9bdaac5bea71b4e090bb2bd9b9f1
Your Name <you@example.com>
2018-09-15 20:40:49 -0700

    added more functionality, tried to fix issue

--- load_images.py
+++ load_images.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import numpy as np
 from operator import mul
 from functools import reduce
-from linearlizer import Linearlizer
+import sys
 mnist = tf.keras.datasets.mnist
+from sklearn import svm    			# To fit the svm classifier\
 
 BATCH_SIZE = 64 #must be at least 2, and bigger is better.
 
@@ -13,9 +14,10 @@ IMAGE_WIDTH = 28
 IMAGE_CHANNELS = 1
 PRE_OUT_DIM = 64
 PRE_CONV_SIZE = 5 # 2 in each direction
-PRE_STRIDE_SIZE = 2 # consider making 2
+PRE_STRIDE_SIZE = 1 # consider making 2
 
 FIRST_CONV_SIZE = 5# 2 in each direction
+FIRST_STRIDE_SIZE = 2
 NUM_CAPS_FIRST_LAYER = 12
 CAPS_SIZE_FIRST_LAYER = 8
 TOT_FIRST_SIZE = NUM_CAPS_FIRST_LAYER * CAPS_SIZE_FIRST_LAYER
@@ -32,7 +34,7 @@ NUM_MISMATCHES = 33
 DISCRIM_HIDDEN_SIZE = 32
 DISCRIM_OUTPUT_SIZE = 16
 
-ADAM_learning_rate = 0.001
+ADAM_learning_rate = 0.0002
 
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train = x_train.astype(np.float32) / 255.0
@@ -41,15 +43,15 @@ x_test = x_test.astype(np.float32) / 255.0
 def make_layer(name,shape):
     tot_size = reduce(mul, shape, 1)
     print(name,tot_size)
-    rand_weight_vals = np.random.randn(tot_size).astype('float32')/(shape[-1]**(0.5**0.5))
+    rand_weight_vals = 0.1*np.random.randn(tot_size).astype('float32')/(shape[-1]**(0.5**0.5))
     rand_weight = np.reshape(rand_weight_vals,shape)
     return tf.Variable(rand_weight,name=name)
 
 def squash_last_dim(arr5d):
-    sqr_size = tf.reduce_sum(arr5d * arr5d,axis=4)
-    sqr_size = tf.reshape(sqr_size,sqr_size.shape.as_list()+[1])
-    epsilon = np.float32(10e-10)
-    return (sqr_size / (np.float32(1.0) + sqr_size)) * (arr5d / tf.maximum(tf.sqrt(sqr_size),epsilon))
+    #sqr_size = tf.reduce_sum(arr5d * arr5d,axis=4)
+    #sqr_size = tf.reshape(sqr_size,sqr_size.shape.as_list()+[1])
+    #epsilon = np.float32(10e-7)
+    return arr5d#(sqr_size / (np.float32(1.0) + sqr_size)) * (arr5d / tf.maximum(tf.sqrt(sqr_size),epsilon))
 
 def make_caps_weights(shape):
     return tf.ones(shape)
@@ -128,7 +130,7 @@ def discriminate_fc(parent_capsule_batch,child_capsule_batch,match_forward_fns):
     print(mat_sel.shape)
     res_vecs = match_forward_fns.multiply_selection(mat_sel,all_parents)
 
-    logit_assignment = tf.nn.sigmoid(tf.reduce_mean(all_children * res_vecs,axis=1)*0.1)
+    logit_assignment = tf.nn.sigmoid(tf.reduce_mean(all_children * res_vecs,axis=1))
     cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=logit_assignment,labels=match_value)
     loss = tf.reduce_mean(cost)
 
@@ -140,12 +142,21 @@ def learn_fn():
 
     in_img = tf.placeholder(tf.float32, (BATCH_SIZE, IMAGE_WIDTH, IMAGE_WIDTH, IMAGE_CHANNELS))
 
+    '''ds = tf.data.Dataset.from_tensor_slices(x_train)
+    ds = ds.repeat(count=10000000000000)
+    ds = ds.shuffle(len(x_train))
+    ds = ds.batch(BATCH_SIZE)
+    ds = ds.map(lambda batch: tf.reshape(batch,(BATCH_SIZE, IMAGE_WIDTH, IMAGE_WIDTH, IMAGE_CHANNELS)))
+    ds = ds.prefetch(8)
+    iter = ds.make_one_shot_iterator()
+    in_img = iter.get_next()'''
+
     pre_conv = make_layer("pre_conv",(PRE_CONV_SIZE, PRE_CONV_SIZE, IMAGE_CHANNELS, PRE_OUT_DIM))
     pre_out = tf.nn.relu(tf.nn.conv2d(in_img,pre_conv,(1,PRE_STRIDE_SIZE,PRE_STRIDE_SIZE,1),CONV_STRATEGY))
 
     lay1conv = make_layer("lay1conv",(FIRST_CONV_SIZE, FIRST_CONV_SIZE, PRE_OUT_DIM, TOT_FIRST_SIZE))
     print(lay1conv.shape)
-    lay1conv_out = tf.nn.conv2d(pre_out,lay1conv,(1,PRE_STRIDE_SIZE,PRE_STRIDE_SIZE,1),CONV_STRATEGY)
+    lay1conv_out = tf.nn.conv2d(pre_out,lay1conv,(1,FIRST_STRIDE_SIZE,FIRST_STRIDE_SIZE,1),CONV_STRATEGY)
     conv_size = lay1conv_out.shape[1]
     capsuled_lay1 = tf.reshape(lay1conv_out,(BATCH_SIZE, conv_size, conv_size, NUM_CAPS_FIRST_LAYER, CAPS_SIZE_FIRST_LAYER))
     squashed_lay1 = squash_last_dim(capsuled_lay1)
@@ -161,37 +172,58 @@ def learn_fn():
     #print(capsuled_lay1.shape)
     #print(lay2_capsules.shape)
     #exit(1)
+    mask = tf.ones((BATCH_SIZE,conv_size*conv_size,NUM_CAPS_FIRST_LAYER))
+    mask = tf.nn.dropout(mask,0.2,noise_shape=(BATCH_SIZE,1,NUM_CAPS_FIRST_LAYER))
+    #with tf.Session() as sess:
+    #    print("mask ",sess.run(mask))
+    mask = tf.reshape(mask,(BATCH_SIZE,conv_size*conv_size,NUM_CAPS_FIRST_LAYER,1))
+    capsuled_lay1 = capsuled_lay1 * mask
     loss = discriminate_fc(capsuled_lay1,lay2_capsules,lay12_match_fns)
     optimizer = tf.train.AdamOptimizer(learning_rate=ADAM_learning_rate)
     optim = optimizer.minimize(loss)
 
-
+    train_data = np.copy(x_train)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        loss_val,opt_val = sess.run([loss,optim],feed_dict={
-            in_img: np.reshape(x_train[:BATCH_SIZE],(BATCH_SIZE,IMAGE_WIDTH, IMAGE_WIDTH, 1))
-        })
-        print(loss_val.shape)
-        print(loss_val)
+        #loss_val,opt_val = sess.run([loss,optim])
+        while True:
+            np.random.shuffle(train_data)
+            loss_sum = 0
+            train_count = 0
+            for x in range(5000):
+                loss_val,opt_val = sess.run([loss,optim],feed_dict={
+                    in_img: np.reshape(train_data[x:x+BATCH_SIZE],(BATCH_SIZE,IMAGE_WIDTH, IMAGE_WIDTH, 1))
+                })
+                loss_sum += loss_val
+                train_count += 1
+            #print(np.any(np.isnan(caps1)))
+            print("loss: {}".format(loss_sum/train_count))
+            all_test_out = []
+            all_train_out = []
+            for x in range(0,1000,BATCH_SIZE):
+                class_out = sess.run(lay2_raw,feed_dict={
+                    in_img: np.reshape(x_test[x:x+BATCH_SIZE],(BATCH_SIZE,IMAGE_WIDTH, IMAGE_WIDTH, 1))
+                })
+                train_out = sess.run(lay2_raw,feed_dict={
+                    in_img: np.reshape(x_train[x:x+BATCH_SIZE],(BATCH_SIZE,IMAGE_WIDTH, IMAGE_WIDTH, 1))
+                })
+                all_test_out.append(class_out)
+                all_train_out.append(train_out)
+
+            tot_test_out = np.concatenate(all_test_out,axis=0)
+            tot_train_out = np.concatenate(all_train_out,axis=0)
+
+            tot_test_cmp = y_test[:len(tot_test_out)]
+            print(tot_test_cmp.shape)
+            tot_train_cmp = y_train[:len(tot_train_out)]
+
+            logit_model = svm.SVC(kernel='linear')
+            logit_model.fit(tot_train_out,tot_train_cmp)
+            print(tot_test_out.shape)
+            print(tot_test_cmp.shape)
+            score = logit_model.score(tot_test_out,tot_test_cmp)
+            print("supervised score: {}".format(score))
+            #print(class_out)
+            sys.stdout.flush()
 
 learn_fn()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
