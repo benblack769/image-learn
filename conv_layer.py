3f32348d9632973ea7922450e3ed821dd7ad610b
Your Name <you@example.com>
2018-09-19 10:50:11 -0400

    added file

---
+++ conv_layer.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+def make_layer(name,shape):
+    tot_size = reduce(mul, shape, 1)
+    print(name,tot_size)
+    rand_weight_vals = np.random.randn(tot_size).astype('float32')/(shape[-1]**(0.5**0.5))
+    rand_weight = np.reshape(rand_weight_vals,shape)
+    return tf.Variable(rand_weight,name=name)
